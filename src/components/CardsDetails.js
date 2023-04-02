bc6e9ddafb4999b3ddfd336d1c192cb1e9de8021
Maya Kasundra <mayakasundra@gmail.com>
2023-04-02 15:45:01 +0530

    first commit

--- src/components/CardsDetails.js
+++ src/components/CardsDetails.js
@@ -6,9 +6,9 @@ import { ADD, DLT, REMOVE } from '../redux/actions/actions';
 
 
 const CardsDetails = () => {
-    const [data,setData] = useState([]);
-    console.log(data)
-  const {id}=useParams();
+  const [data, setData] = useState([]);
+  console.log(data)
+  const { id } = useParams();
   const history = useNavigate();
 
   const dispatch = useDispatch();
@@ -16,89 +16,89 @@ const CardsDetails = () => {
     return state.cartReducer.carts;
 
 
-})
-const compare=()=>{
-  let compareData = getData.filter((e)=>{
-    return e.id == id
-})  ;
-setData(compareData);
-}
+  })
+  const compare = () => {
+    let compareData = getData.filter((e) => {
+      return e.id == id
+    });
+    setData(compareData);
+  }
 
-const send = (e)=>{
-  // console.log(e);
-  dispatch(ADD(e));
-}
-
-
-const dlt = (id)=>{
-  dispatch(DLT(id));
-  history("/")
-
- 
-}
-
-const remove = (item)=>{
-  dispatch(REMOVE(item))
-}
+  const send = (e) => {
+    // console.log(e);
+    dispatch(ADD(e));
+  }
 
 
-// console.log(compareData);
-  
-   useEffect(()=>{
+  const dlt = (id) => {
+    dispatch(DLT(id));
+    history("/")
+
+
+  }
+
+  const remove = (item) => {
+    dispatch(REMOVE(item))
+  }
+
+
+  // console.log(compareData);
+
+  useEffect(() => {
     compare();
-   },[id])
+  }, [id])
   return (
-   <>
-    <div className="container mt-2">
+    <>
+      <div className="container mt-2">
         <h2 className='text-center'>Items Details Page</h2>
         <section className="container mt-3">
-        <div className="iteamsdetails">
-        {
-          data.map((ele)=>{
-            return(
-              <>
-              <div className="items_img">
-              <img src={ele.imgdata} alt="" />
-            </div>
-            <div className="details">
-            <Table>
-                <tr>
-                <td>
-                    <p> <strong>{ele.rname}</strong>  : </p>
-                    <p> <strong>{ele.price}</strong>  : </p>
-                    <p> <strong>Dishes : {ele.address}</strong>  :</p>
-                    <p> <strong>Total: {ele.price * ele.qnty}</strong></p>
-                    <div className='mt-5 d-flex justify-content-between align-items-center' style={{width:100,cursor:"pointer",background:"#ddd",color:"#111"}}>
-                    <span style={{fontSize:24}} onClick={ele.qnty < 1 ? dlt(ele.id): ()=>remove(ele)} >-</span>
-                    <span style={{fontSize:22}}>{ele.qnty}</span> 
-                    <span style={{fontSize:24}} onClick={()=>send(ele)}>+</span>
+          <div className="iteamsdetails">
+            {
+              data.map((ele) => {
+                return (
+                  <>
+                    <div className="items_img">
+                      <img src={ele.imgdata} alt="" />
+                    </div>
+                    <div className="details">
+                      <Table>
+                        <tr>
+                          <td>
+                            <p> <strong>{ele.rname}</strong>  : </p>
+                            <p> <strong>{ele.price}</strong>  : </p>
+                            <p> <strong>Dishes : {ele.address}</strong>  :</p>
+                            <p> <strong>Total: {ele.price * ele.qnty}</strong></p>
+                            <div className='mt-5 d-flex justify-content-between align-items-center' style={{ width: 100, cursor: "pointer", background: "#ddd", color: "#111" }}>
+                              <span style={{ fontSize: 24 }} onClick={ele.qnty < 1 ? dlt(ele.id) : () => remove(ele)} >-</span>
+                              <span style={{ fontSize: 22 }}>{ele.qnty}</span>
+                              <span style={{ fontSize: 24 }} onClick={() => send(ele)}>+</span>
+
+                            </div>
+
+                          </td>
+                          <td>
+                            <p><strong>Rating :</strong> <span style={{ background: "green", color: "#fff", padding: "2px 5px", borderRadius: "5px" }}> {ele.rating}★	</span></p>
+                            <p><strong>Order Review : {ele.somedata}</strong> <span >	</span></p>
+                            <p><strong>Remove :</strong> <span ><i className='fas fa-trash' style={{ color: "red", fontSize: 20, cursor: "pointer" }} onClick={() => dlt(ele.id)}></i>	</span></p>
+                          </td>
+                        </tr>
+                      </Table>
 
                     </div>
 
-                  </td>
-                  <td>
-                    <p><strong>Rating :</strong> <span style={{background:"green",color:"#fff",padding:"2px 5px",borderRadius:"5px"}}> {ele.rating}★	</span></p>
-                    <p><strong>Order Review : {ele.somedata}</strong> <span >	</span></p>
-                    <p><strong>Remove :</strong> <span ><i className='fas fa-trash'  style={{color:"red",fontSize:20,cursor:"pointer"}} onClick={()=>dlt(ele.id)}></i>	</span></p>
-                  </td>
-                </tr>
-            </Table>
 
-            </div>
-            
+                  </>
+                )
+              })
+            }
 
-              </>
-            )
-          })
-        }
-          
-            
-                
-            </div>
+
+
+          </div>
 
         </section>
-    </div>
-   </>
+      </div>
+    </>
   )
 }
 
--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,56 +1,57 @@
 const INIT_STATE = {
-    carts:[]
-  
+    carts: []
+
 };
-export const cartReducer =(state=INIT_STATE, action)=>{
-    switch(action.type){
-        
-        case "ADD_CART" :
-            // console.log(state);
-            
-        const itemIndex = state.carts.findIndex((item)=> item.id === action.payload.id);
+export const cartReducer = (state = INIT_STATE, action) => {
+    switch (action.type) {
 
-        if(itemIndex >= 0){
-            state.carts[itemIndex].qnty +=1
-            return {
-                ...state,
-                carts:[...state.carts]
-            }
-        }else{
-            const temp = {...action.payload,qnty:1}
-             return {
-                ...state,
-                carts: [...state.carts, temp]
-            }
-        }
+        case "ADD_CART":
+            console.log("state", state);
 
-            case "RMV_CART":
-            const data = state.carts.filter((el)=>el.id !== action.payload); 
+            const itemIndex = state.carts.findIndex((item) => item.id === action.payload.id);
+            console.log("itemindex", itemIndex)
+
+            if (itemIndex >= 0) {
+                state.carts[itemIndex].qnty += 1
+                return {
+                    ...state,
+                    carts: [...state.carts]
+                }
+            } else {
+                const temp = { ...action.payload, qnty: 1 }
+                return {
+                    ...state,
+                    carts: [...state.carts, temp]
+                }
+            }
+
+        case "RMV_CART":
+            const data = state.carts.filter((el) => el.id !== action.payload);
             // console.log(data);
 
             return {
                 ...state,
-                carts:data
+                carts: data
             }
 
-        
-            case "RMV_ONE":
-            const IteamIndex_dec = state.carts.findIndex((iteam)=> iteam.id === action.payload.id);
-   
-            if(state.carts[IteamIndex_dec].qnty >= 1){
+
+        case "RMV_ONE":
+            const IteamIndex_dec = state.carts.findIndex((iteam) => iteam.id === action.payload.id);
+
+            if (state.carts[IteamIndex_dec].qnty >= 1) {
                 const dltiteams = state.carts[IteamIndex_dec].qnty -= 1
-                console.log([...state.carts,dltiteams]);
+                console.log([...state.carts, dltiteams]);
 
                 return {
                     ...state,
-                    carts:[...state.carts]
+                    carts: [...state.carts]
                 }
-            }else if(state.carts[IteamIndex_dec].qnty === 1 ){
-                const data = state.carts.filter((el)=>el.id !== action.payload);
+            } else if (state.carts[IteamIndex_dec].qnty === 1) {
+                const data = state.carts.filter((el) => el.id !== action.payload);
 
                 return {
                     ...state,
-                    carts:data
+                    carts: data
                 }
             }
 
